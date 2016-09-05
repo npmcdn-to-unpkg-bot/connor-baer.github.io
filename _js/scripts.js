c7e264e0ebf6b23bebd466c72482ff40261721d4
Connor Bär <hello@connorbaer.io>
2016-09-05 18:57:56 +0200

    Fixes js code style errors.

--- _js/scripts.js
+++ _js/scripts.js
@@ -1,24 +1,31 @@
 // HEADROOM //
 
-// grab an element
+// Grab an element.
 var hrBody = document.body;
-// construct an instance of Headroom, passing the element
-var headroom  = new Headroom(hrBody, {
-    // vertical offset in px before element is first unpinned
+
+// Construct an instance of Headroom, passing the element.
+var headroom  = new Headroom( hrBody, {
+
+    // Vertical offset in px before element is first unpinned.
     offset: 64,
-    // scroll tolerance in px before state changes for up/down scroll
+
+    // Scroll tolerance in px before state changes for up/down scroll.
     tolerance: {
         up: 10,
         down: 5
     }
 });
-// initialise
-headroom.init();
 
+// Initialise.
+headroom.init();
 
 // SMOOTHSCROLL //
 
 smoothScroll.init({
-	easing: 'easeInOutCubic', // Easing pattern to use
-	offset: 70,  // How far to offset the scrolling anchor location in pixels
+
+	// Easing pattern to use.
+	easing: 'easeInOutCubic',
+
+	// How far to offset the scrolling anchor location in pixels.
+	offset: 70
 });
